a68c06d6b2fc53b8da6286a16c872365307a6d02
snaco <jm.anderson.cs@gmail.com>
2021-12-10 15:52:43 -0500

    added docstring to entities.__init__

--- src/entities/__init__.py
+++ src/entities/__init__.py
@@ -1,3 +1,4 @@
+"""import to export to import more easily"""
 from entities.monkey_handler import MonkeyHandler
 from entities.monkey_response import MonkeyResponse
 from entities.mnkc_config import MonkeySeeConfig
